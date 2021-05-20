5ab5809183b072fdfb129b211679682e249761ac
Sune Carelse <sune.carelse@takealot.com>
2021-05-20 22:28:29 +0200

    yarn and cleanup

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+        <div>
+
+        </div>
+{/*middle  */}
+        <div>
+
+        </div>
+    </header>
+  );
+}
+
+export default Header;
