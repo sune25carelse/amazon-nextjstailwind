8963eadcd1409fef681fa1e26624e32bffe5cb40
Sune Carelse <sune.carelse@takealot.com>
2021-05-20 22:39:00 +0200

    where we getting the images from  fakestr api

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,20 @@
-import React from "react";
+import Image from "next/image";
 
 function Header() {
   return (
     <header>
+      {/* top nav */}
+      <div>
         <div>
-
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+          />
         </div>
-{/*middle  */}
-        <div>
-
-        </div>
+      </div>
+      {/*bottom nav  */}
+      <div></div>
     </header>
   );
 }
