0666edecdf12498d6ef4f287bb3fda15eaf3526a
Sune Carelse <sune.carelse@takealot.com>
2021-05-24 21:15:44 +0200

    tailwindcss corrected

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
