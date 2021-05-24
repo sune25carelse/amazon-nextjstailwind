a12e7dc610a5fa4187eb43c74c9068c029f970df
Sune Carelse <sune.carelse@takealot.com>
2021-05-24 21:35:17 +0200

    right side basket and number

--- components/Header.js
+++ components/Header.js
@@ -30,19 +30,24 @@ function Header() {
         </div>
         {/* rightside */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap ">
-          <div className="cursor-pointer link">
-            <p>Luca Van Wyk</p>
-            <p>Account & lists</p>
+          <div className="link">
+            <p>Hello Luca Van Wyk</p>
+            <p className="font-extra-bold md:text-sm">Account & lists</p>
           </div>
 
-          <div className="cursor-pointer link">
+          <div className="link">
             <p>Returns</p>
-            <p>& Orders</p>
+            <p className="font-extra-bold md:text-sm">& Orders</p>
           </div>
 
-          <div className="cursor-pointer link">
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
             <ShoppingCartIcon className="h-10" />
-            <p>Basket</p>
+            <p className="hidden md:inline font-extra-bold md:text-sm mt-2">
+              Basket
+            </p>
           </div>
         </div>
       </div>
