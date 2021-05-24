ab37be755747d2d670de5011555eaa9d101b23f8
Sune Carelse <sune.carelse@takealot.com>
2021-05-24 21:15:31 +0200

    rightside of header

--- components/Header.js
+++ components/Header.js
@@ -21,9 +21,29 @@ function Header() {
         </div>
 
         {/* Search */}
-        <div className="hidden bg-yellow-400 hover:bg-yellow-500">
-          <input type="text" />
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none "
+            type="text"
+          />
           <SearchIcon className="h-12 p-4" />
+        </div>
+        {/* rightside */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap ">
+          <div className="cursor-pointer link">
+            <p>Luca Van Wyk</p>
+            <p>Account & lists</p>
+          </div>
+
+          <div className="cursor-pointer link">
+            <p>Returns</p>
+            <p>& Orders</p>
+          </div>
+
+          <div className="cursor-pointer link">
+            <ShoppingCartIcon className="h-10" />
+            <p>Basket</p>
+          </div>
         </div>
       </div>
 
