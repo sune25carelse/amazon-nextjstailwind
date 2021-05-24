d397e51f4168ea37b676475c6e7e092180e66e4c
Sune Carelse <sune.carelse@takealot.com>
2021-05-24 22:26:51 +0200

    now products being pulled in

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Banner from "../../components/Banner";
 import Header from "../../components/Header";
 import ProductFeed from "../../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -17,7 +17,7 @@ export default function Home() {
         <Banner />
 
         {/* product feed */}
-        <ProductFeed />
+        <ProductFeed products={products} />
       </main>
     </div>
   );
@@ -27,6 +27,12 @@ export async function getServerSideProps(context) {
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
 
 // GET >> https://fakestoreapi.com/products
