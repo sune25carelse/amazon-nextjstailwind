57032413a072e424f8b7de8feadd2caf50073c39
Sune Carelse <sune.carelse@takealot.com>
2021-05-24 22:19:46 +0200

    fetch products api from fake store

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../../components/Banner";
 import Header from "../../components/Header";
+import ProductFeed from "../../components/ProductFeed";
 
 export default function Home() {
   return (
@@ -16,7 +17,16 @@ export default function Home() {
         <Banner />
 
         {/* product feed */}
+        <ProductFeed />
       </main>
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+}
+
+// GET >> https://fakestoreapi.com/products
