f9e42ea8810604177dbfb90b8bbcb74473683baa
Sune Carelse <sune.carelse@takealot.com>
2021-05-25 21:55:41 +0200

    grid products when going biggr scrns

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -2,8 +2,7 @@ import Product from "./Product";
 
 function ProductFeed({ products }) {
   return (
-    <div>
-      <h1>Products...</h1>
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52">
       {products.map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
