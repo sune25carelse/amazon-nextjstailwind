72ce375a2a0e10f45f4036aa6199c05eda65490d
Sune Carelse <sune.carelse@takealot.com>
2021-05-26 21:34:46 +0200

    now whole app has next auth state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,15 @@ import { Provider } from "react-redux";
 import { store } from "../app/store";
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import { Provider as AuthProvider } from "next-auth/client";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <AuthProvider session={pageProps.session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </AuthProvider>
   );
 };
 
