6eb86c2e76883b1352171a8a7c276cabf75e8765
Sune Carelse <sune.carelse@takealot.com>
2021-05-26 21:59:33 +0200

    onclick route page from home to basket

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,11 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 function Header() {
   const [session] = useSession();
+  const router = useRouter();
 
   return (
     <header>
@@ -15,6 +17,7 @@ function Header() {
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -48,7 +51,10 @@ function Header() {
             <p className="font-extra-bold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative flex items-center cursor-pointer link">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="relative flex items-center cursor-pointer link"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
               0
             </span>
