146440d97afd256679c5e1810833a017d19e324c
Sune Carelse <sune.carelse@takealot.com>
2021-05-26 20:50:27 +0200

    yarn firebase, app creatd register without hosting

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ1m6wqrR2viV4Xf8U7hbB2UC5NsOxjBc",
+  authDomain: "nextjstailw.firebaseapp.com",
+  projectId: "nextjstailw",
+  storageBucket: "nextjstailw.appspot.com",
+  messagingSenderId: "807960834145",
+  appId: "1:807960834145:web:e56379641ddbc9b234fa20",
+};
