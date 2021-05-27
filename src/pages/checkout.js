e03e5a1ecc4e25ef0ece0c18d9a2d3a4762e5f25
Sune Carelse <sune.carelse@takealot.com>
2021-05-27 21:55:33 +0200

    add item to basket in the redux store

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,6 +5,7 @@ function checkout() {
   return (
     <div className="bg-gray-100">
       <Header />
+
       <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* left */}
         <div className="flex-grow m-5 shadow-sm">
@@ -14,6 +15,7 @@ function checkout() {
             height={250}
             objectFit="contain"
           />
+
           <div className="flex flex-col p-5 space-y-10 bg-white">
             <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
           </div>
