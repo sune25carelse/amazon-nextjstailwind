311ceeb171cad79534c41e2c3153fb1b2ec4a903
Sune Carelse <sune.carelse@takealot.com>
2021-05-27 21:09:00 +0200

    checkout pg banner done

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,10 +1,27 @@
+import Image from "next/image";
 import Header from "../../components/Header";
 
 function checkout() {
   return (
     <div className="bg-gray-100">
       <Header />
-      <h1>checkout page</h1>
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* left */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div></div>
+      </main>
     </div>
   );
 }
