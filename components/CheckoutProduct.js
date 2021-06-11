926522ca4a52d8dda0a38cbb23b4e1f9044503e0
Sune Carelse <sune.carelse@takealot.com>
2021-06-11 22:32:03 +0200

    push into redux store

--- components/CheckoutProduct.js
+++ components/CheckoutProduct.js
@@ -1,46 +1,46 @@
-import { StarIcon } from "@heroicons/react/solid";
-import Image from "next/image";
-import Currency from "react-currency-formatter";
 import { useDispatch } from "react-redux";
 import { addToBasket, removeFromBasket } from "../src/slices/basketSlice";
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
 
 function CheckoutProduct({
   id,
   title,
   price,
+  rating,
   description,
   category,
   image,
-  rating,
   hasPrime,
 }) {
   const dispatch = useDispatch();
 
   const addItemToBasket = () => {
-    const product = {
-      id,
-      title,
-      price,
-      description,
-      category,
-      image,
-      rating,
-      hasPrime,
-    };
-
-    // push item into redux
-    dispatch(addToBasket(product));
+    //  push item into redux
+    dispatch(
+      addToBasket({
+        id,
+        title,
+        price,
+        rating,
+        description,
+        category,
+        image,
+        hasPrime,
+      })
+    );
   };
 
   const removeItemFromBasket = () => {
-    // remove item from basket
+    // remove from checkout
     dispatch(removeFromBasket({ id }));
   };
-
   return (
     <div className="grid grid-cols-5">
-      <Image src={image} width={100} height={300} objectFit="contain" />
+      <Image src={image} height={200} width={200} objectFit="contain" />
 
+      {/* middle */}
       <div className="col-span-3 mx-5">
         <p>{title}</p>
         <div className="flex">
@@ -50,10 +50,8 @@ function CheckoutProduct({
               <StarIcon key={i} className="h-5 text-yellow-500" />
             ))}
         </div>
-
-        <p className="text-xs my-2 line-clamp-3">{description}</p>
+        <p className="text-xs mt-2 mb-2 line-clamp-3">{description}</p>{" "}
         <Currency quantity={price} currency="ZAR" />
-
         {hasPrime && (
           <div className="flex items-center space-x-2">
             <img
