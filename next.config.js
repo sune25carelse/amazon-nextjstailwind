961cf48aa445aaa3e10dac25fac2dee9d42bda61
Sune Carelse <sune.carelse@takealot.com>
2021-06-17 21:17:02 +0200

    loadStripe for public key only

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
+  // only for publioc key
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+  },
 };
